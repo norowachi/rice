2b7ccae67ce8fa93907d2b8941db37557f1372b8
Andrew <contact@noro.cc>
2025-10-30 14:43:37 +0100

    fix: make satty save to file on copy, before upload

--- .bin/zipline.sh
+++ .bin/zipline.sh
@@ -13,4 +13,4 @@ ZIPLINE_TOKEN="";
 
 upload="curl -H \"authorization: $ZIPLINE_TOKEN\" https://cdn.noro.cc/api/upload -F file=@/tmp/ss.png -H \"Content-Type: multipart/form-data\" | jq -r .files[0].url | tr -d '\n' | wl-copy";
 
-satty --config "$HOME/.config/satty/zipline.toml" -f "$screenshot" --copy-command "$upload";
+satty --config "$HOME/.config/satty/zipline.toml" -f "$screenshot" --copy-command "tee /tmp/ss.png && $upload";
